012ffecdef5bdaac9f8773cbc335c91112700ef4
OndrejMelichar <42346135+OndrejMelichar@users.noreply.github.com>
2019-04-14 16:13:56 +0200

    nic

--- MelicharVLC/MainWindow.xaml.cs
+++ MelicharVLC/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,12 @@ namespace MelicharVLC
         public MainWindow()
         {
             InitializeComponent();
+            var currentAssembly = Assembly.GetEntryAssembly();
+            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+            var libDirectory = new DirectoryInfo(@"C:\Program Files\VideoLAN\VLC\vlc.exe");
+
+            this.VlcControl.SourceProvider.CreatePlayer(libDirectory/* pass your player parameters here */);
+            this.VlcControl.SourceProvider.MediaPlayer.Play(new Uri("http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
         }
     }
 }
